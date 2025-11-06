bd1176a98590078ec31c00b9ac93cb3a7fd3b191
Francisco Silva <metroproyect@gmail.com>
2025-11-06 16:13:49 -0600

    boton salir listo

--- NewFront/angularprj/frontalm5/src/app/component/menu/menu.component.ts
+++ NewFront/angularprj/frontalm5/src/app/component/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,7 +7,25 @@ import { Router } from '@angular/router';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  usuarioNombre: string = '';
+
   constructor(public router:Router){}
+
+  ngOnInit(): void {
+    const usuarioStr = localStorage.getItem('usuario');
+    if (usuarioStr) {
+      const usuario = JSON.parse(usuarioStr);
+      this.usuarioNombre = usuario.nombre;
+    }
+  }
+
+  logout(): void {
+    if (confirm('¿Seguro que deseas cerrar sesión?')) {
+      localStorage.removeItem('usuario'); // 🔹 Elimina la sesión
+      this.router.navigate(['/login']);   // 🔹 Redirige al login
+    }
+  }
 
 }
